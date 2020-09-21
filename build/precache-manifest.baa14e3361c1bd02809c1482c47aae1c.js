5584427988fac32f3382d1682830ae5e83cf943e
Arthur Ruan <arthur123ruan@hotmail.com>
2020-09-21 10:51:22 -0300

    heroku in deploy

---
+++ build/precache-manifest.baa14e3361c1bd02809c1482c47aae1c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cedf95297a04706185c1167071e09f97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b17be51cffc2631be705",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "52170050dda7bc8753f1",
+    "url": "/static/css/main.122ab22d.chunk.css"
+  },
+  {
+    "revision": "b17be51cffc2631be705",
+    "url": "/static/js/2.1395e376.chunk.js"
+  },
+  {
+    "revision": "c7fcf0d3f03fb93ad8ef3361b5f22c4e",
+    "url": "/static/js/2.1395e376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52170050dda7bc8753f1",
+    "url": "/static/js/main.202c94c2.chunk.js"
+  },
+  {
+    "revision": "d31c781128ab9f32b7b5",
+    "url": "/static/js/runtime-main.812f8336.js"
+  },
+  {
+    "revision": "99879645e436e0bf1341bd577c4ef001",
+    "url": "/static/media/coding.99879645.svg"
+  },
+  {
+    "revision": "5785076f4a03fdad1b333cdc47b8a9ff",
+    "url": "/static/media/illustration.5785076f.jpg"
+  },
+  {
+    "revision": "5e4426182723a94798f8adae155eeb2e",
+    "url": "/static/media/logo.5e442618.png"
+  },
+  {
+    "revision": "bc7b1ee30359bc0c2cfb64697cfa67e4",
+    "url": "/static/media/noLogged.bc7b1ee3.svg"
+  },
+  {
+    "revision": "f381ed6c48cd789b9011f5c8fcb0b00e",
+    "url": "/static/media/noUser.f381ed6c.png"
+  },
+  {
+    "revision": "f1017bcd08c4881d11cc1760756b4f77",
+    "url": "/static/media/waveFooter.f1017bcd.svg"
+  }
+]);
